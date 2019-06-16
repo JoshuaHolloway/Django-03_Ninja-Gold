1ca88c383f9e79452fe78834fbca09e8e7eb4397
JoshuaHolloway <jhollow6@asu.edu>
2019-06-16 08:57:33 -0500

    removed unnecessary jinja usage

--- ninja_gold_project/ninja_gold_project/settings.py
+++ ninja_gold_project/ninja_gold_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.ninja_gold_app',    # JOSH: added
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
