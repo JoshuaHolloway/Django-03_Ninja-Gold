3c14a0a7cae68b20f90ea4b6a55491fccf06b2b3
JoshuaHolloway <jhollow6@asu.edu>
2019-06-16 14:26:30 -0500

    logging the buttons pressed with color

--- ninja_gold_project/apps/ninja_gold_app/views.py
+++ ninja_gold_project/apps/ninja_gold_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
 from random import randint
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     request.session['name'] = 'JOSH'
-    request.session['gold_sum'] = 0
+
+    if 'gold_sum' not in request.session:
+        request.session['gold_sum'] = 0
+    request.session['activities'] = []
 
     # if request.method == "GET":
     #     print("a GET request is being made to this route")
@@ -14,44 +18,33 @@ def index(request):
         val_from_field_two = request.POST["two"]
         print(val_from_field_one)
         print(val_from_field_two)
-
     return render(request, "ninja_gold_app/index.html")
-
-
+# ============================================================
 def process(request):
-
-    # Initialize stuff
+    building = request.POST['building']
+    print(building) # DEBUG
     gold = 0
-    building = ""
-
-    if request.POST['building'] == 'farm':
-        print("FARM")  # DEBUG
-        request.session['name'] = 'YOU PRESSED FARM!'
-        gold = randint(10,21) #rand-num between 10-20
-        building = 'farm'
-
-    if request.POST['building'] == 'cave':
-        print("CAVE")  # DEBUG
-        request.session['name'] = 'YOU PRESSED CAVE!'
-        gold = randint(5,11) #rand-num between 5-10
-        building = 'cave'
-
-    if request.POST['building'] == 'house':
-        print("HOUSE")  # DEBUG
-        request.session['name'] = 'YOU PRESSED HOUSE!'
-        gold = randint(2,6) #rand-num between 2-5
-        building = 'house'
-
-    if request.POST['building'] == 'casino':
-        print("CASINO")  # DEBUG
-        request.session['name'] = 'YOU PRESSED CASINO!'
-        gold = randint(-100,50) #rand-num between -100:50
-        building = 'casino'
-
+    if building == 'farm':
+        gold = randint(10, 21)  # rand-num between 10-20
+    if building == 'cave':
+        gold = randint(5, 11)  # rand-num between 5-10
+    if building == 'house':
+        gold = randint(2, 6)  # rand-num between 2-5
+    if building == 'casino':
+        gold = randint(-100, 50)  # rand-num between -100:50
 
     # request.session['activities'].append({"result": result, "color": color})
     request.session['gold_sum'] += gold
 
+    result = ''
+    if gold < 0:
+        result = f'Lost {abs(gold)} golds from {building}, {datetime.now()}'
+        color = "text-danger"
+    else:
+        result = f'Earned {abs(gold)} golds from {building}, {datetime.now()}'
+        color = "text-success"
+
+    request.session['activities'].append({"result": result, "color": color})
 
     # request.session['name'] = request.POST['name']
     request.session['counter'] = 100
