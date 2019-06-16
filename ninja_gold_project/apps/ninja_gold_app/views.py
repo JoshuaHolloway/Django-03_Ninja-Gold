024ad20b48164435f11f31455d0f627371581505
JoshuaHolloway <jhollow6@asu.edu>
2019-06-16 13:09:29 -0500

    fully wired up all buttons

--- ninja_gold_project/apps/ninja_gold_app/views.py
+++ ninja_gold_project/apps/ninja_gold_app/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render, HttpResponse, redirect
 def index(request):
     request.session['name'] = 'JOSH'
 
-    # if request.POST['building'] == 'farm':
-    #     request.session['name'] = 'FARM'
-
     # if request.method == "GET":
     #     print("a GET request is being made to this route")
     if request.method == "POST":
@@ -14,10 +11,31 @@ def index(request):
         val_from_field_one = request.POST["one"]
         val_from_field_two = request.POST["two"]
         print(val_from_field_one)
+        print(val_from_field_two)
 
     return render(request, "ninja_gold_app/index.html")
-    #return HttpResponse("Hello Multiverse!")
 
-def some_function(request):
+
+def process(request):
+    print("You pressed a 'Find-Gold!' button")
+    if request.POST['building'] == 'farm':
+        request.session['name'] = 'YOU PRESSED FARM!'
+        print("FARM")
+
+    if request.POST['building'] == 'cave':
+        request.session['name'] = 'YOU PRESSED CAVE!'
+        print("FARM")
+
+    if request.POST['building'] == 'house':
+        request.session['name'] = 'YOU PRESSED HOUSE!'
+        print("FARM")
+
+    if request.POST['building'] == 'casino':
+        request.session['name'] = 'YOU PRESSED CASINO!'
+        print("FARM")
+
     # request.session['name'] = request.POST['name']
     request.session['counter'] = 100
+    return render(request, "ninja_gold_app/index.html")
+    #redirect("/")
+    # return HttpResponse("Hello Multiverse!")
