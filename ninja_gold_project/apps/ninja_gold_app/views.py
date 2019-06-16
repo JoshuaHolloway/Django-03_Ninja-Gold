385a833ad34e2356ddbd7a0447440219a0dcc93b
JoshuaHolloway <jhollow6@asu.edu>
2019-06-16 13:26:47 -0500

    accumulating gold from button presses

--- ninja_gold_project/apps/ninja_gold_app/views.py
+++ ninja_gold_project/apps/ninja_gold_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
+from random import randint
 
 # Create your views here.
 def index(request):
     request.session['name'] = 'JOSH'
+    request.session['gold_sum'] = 0
 
     # if request.method == "GET":
     #     print("a GET request is being made to this route")
@@ -17,22 +19,39 @@ def index(request):
 
 
 def process(request):
-    print("You pressed a 'Find-Gold!' button")
+
+    # Initialize stuff
+    gold = 0
+    building = ""
+
     if request.POST['building'] == 'farm':
+        print("FARM")  # DEBUG
         request.session['name'] = 'YOU PRESSED FARM!'
-        print("FARM")
+        gold = randint(10,21) #rand-num between 10-20
+        building = 'farm'
 
     if request.POST['building'] == 'cave':
+        print("CAVE")  # DEBUG
         request.session['name'] = 'YOU PRESSED CAVE!'
-        print("FARM")
+        gold = randint(5,11) #rand-num between 5-10
+        building = 'cave'
 
     if request.POST['building'] == 'house':
+        print("HOUSE")  # DEBUG
         request.session['name'] = 'YOU PRESSED HOUSE!'
-        print("FARM")
+        gold = randint(2,6) #rand-num between 2-5
+        building = 'house'
 
     if request.POST['building'] == 'casino':
+        print("CASINO")  # DEBUG
         request.session['name'] = 'YOU PRESSED CASINO!'
-        print("FARM")
+        gold = randint(-100,50) #rand-num between -100:50
+        building = 'casino'
+
+
+    # request.session['activities'].append({"result": result, "color": color})
+    request.session['gold_sum'] += gold
+
 
     # request.session['name'] = request.POST['name']
     request.session['counter'] = 100
